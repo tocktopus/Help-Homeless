743d4c7b930602ae81de3aef0580d0daee1a6b01
Sara <89416100+tocktopus@users.noreply.github.com>
2021-11-30 16:26:59 -0300

    Add files via upload

---
+++ help-homeless/components/Navegacao.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+      <b-navbar toggleable="lg" type="dark" variant="danger">
+          <b-navbar-brand href="#">Doadores</b-navbar-brand>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+                <b-button to="/formDoador" size="sm" class="mr-sm-2">
+                    Novo Doador
+                </b-button>
+            </b-nav-form>
+
+            <b-nav-form>
+                <b-button to="/doador" size="sm" class="mr-sm-2">
+                    Todos os Doadores
+                </b-button>
+            </b-nav-form>
+          </b-navbar-nav>
+      </b-navbar>
+   </div>
+</template>
